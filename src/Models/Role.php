a69abf03bc90d6d0f49dbce0efd9573c370b92c8
Novato Pro <admin@novato.pro>
2023-05-08 23:37:02 -0500

    Add Role Model

---
+++ src/Models/Role.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NovatoPro\Lrp\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'name',
+        'slug',
+    ];
+
+    /**
+     * The users that belong to the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
